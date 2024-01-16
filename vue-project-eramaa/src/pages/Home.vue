c0f99ac4f43007bc00c1d2efc9d33cf1f211c98a
Deimar <deimarparn14@gmail.com>
2024-01-16 17:35:50 +0200

    Created the first section of home page

--- vue-project-eramaa/src/pages/Home.vue
+++ vue-project-eramaa/src/pages/Home.vue
@@ -1,12 +1,60 @@
 <template>
-    <div>
+    <div class="main-container">
+        <div class="banner-wrapper">
+            <div class="filter-overlay"></div>
+                <video ref="videoPlayer" style="height: 1920; width: 1080;">
+                    <source :src="videoSource" type="video/mp4" />
+                </video>
+            <div class="banner-container">
+                <div class="grid-wrapper">
+                    <div class="banner-item-left">
+                        <h2><span>Sinu partner </span>põllu- ja metsamajandamisel</h2>
+                        <p>Majandame Eesti metsi vastutustundlikult ning lisame kasutamata põldudele väärtust.
+                        Järeltulevatele põlvedele jätame jätkusuutlikudmetsad ja põllumaad.
+                        </p>
+                    </div>
+                    <div class="banner-item-right">
+                    </div>
+                </div>
+                <div class="banner-footer-layout">
+                    <div class="layout-item">
+                        <h3>Personaalne ja paindlik</h3>
+                        <p>Tagame maaomanikule maksimaalse tulu. Majanduskavade ja raieplaanide koostamine on kohandatav vastavalt kliendi soovidele.</p>
+                    </div>
+                    <div class="layout-item">
+                        <h3>Kvaliteet ja tipptapsemel tehnika</h3>
+                        <p>Eramaas töötavad kogenud spetsialistid ja meil on kaasaegne masinapark. Hindame oma töös kõrgelt korrektsust ja läbipaistvust.</p>
+                    </div>
+                    <div class="layout-item">
+                        <h3>Maaomanike harimine</h3>
+                        <p>Metsade ja põllumaade teadlik ja majanduslikult tõhusmajandamine. Räägime läbi oma kogemuse, miks on oluline metsa majandada ning kuidas seda teha.</p>
+                    </div>  
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+
 export default {
     name: 'Home',
+    data() {
+        return {
+            videoSource: '/Videos/-baa4-41f9-8812-4d63432e65f9.mp4'
+        };
+    },
+
+    mounted() {
+        this.$refs.videoPlayer.addEventListener('loadedmetadata', this.playVideo);
+    },
+    methods: {
+        playVideo() {
+            this.$refs.videoPlayer.play();
+        },
+    }
 }
+
 </script>
 
 <style lang="scss">
