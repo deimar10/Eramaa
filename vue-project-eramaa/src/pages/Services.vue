0ad50e20c5f9b464aa726b433ba383d83b09e800
Deimar <deimarparn14@gmail.com>
2024-01-19 14:11:20 +0200

    Implemented servcises banner container

--- vue-project-eramaa/src/pages/Services.vue
+++ vue-project-eramaa/src/pages/Services.vue
@@ -1,10 +1,27 @@
 <template>
-
+    <div class="services-banner-wrapper">
+        <Navbar />
+        <div class="services-filter-overlay"></div>
+            <img src="/Images/close-up-park-rangers-woods.png">
+            <div class="services-banner-img-container">
+                <img src="/Images/variant 2.png"> 
+            </div>
+        <div class="services-banner-container">
+            <div class="services-banner-text">
+                <h2>Teenused</h2>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue';
+
 export default {
     name: 'Services',
+    components: {
+        Navbar,
+    }
 }
 </script>
 
