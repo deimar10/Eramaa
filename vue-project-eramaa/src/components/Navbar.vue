2543903c96ee3657ceecae36a824a087131e4b9d
Deimar <deimarparn14@gmail.com>
2024-01-19 19:22:39 +0200

    Added routing to home page

--- vue-project-eramaa/src/components/Navbar.vue
+++ vue-project-eramaa/src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <div :class="{ 'nav-main-container' : true, 'sticky'  : isSticky}">
         <ul>
             <div class="nav-image-container">
-                <img src="/Images/Eramaa_logo_dark-green_without-safezone_RGB.svg" width="234" height="36">
+                <router-link to="/"><img src="/Images/Eramaa_logo_dark-green_without-safezone_RGB.svg" width="234" height="36"></router-link>
             </div>
             <div class="nav-list-container">
                 <li><router-link to="/services">Teenused</router-link></li>
