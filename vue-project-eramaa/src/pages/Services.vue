edcedeb384723624010a53c7125b368d7a2bd16f
Deimar <deimarparn14@gmail.com>
2024-01-19 16:14:39 +0200

    Implemented detail maping

--- vue-project-eramaa/src/pages/Services.vue
+++ vue-project-eramaa/src/pages/Services.vue
@@ -12,15 +12,35 @@
             </div>
         </div>
     </div>
+    <div class="services-detail-container">
+        <div class="detail-grid-container" v-for="(data, index) in serviceData" :key="index">
+            <div class="detail-grid-item" v-if="index < serviceData.length - 1" :class="{ 'order-reverse' : index % 2 !== 0 }">
+                <img :src="data.image">
+            </div>
+            <div class="detail-grid-item" v-if="index < serviceData.length - 1">
+                <h3>{{ data.title }}</h3>
+                <p>{{ data.text }}</p>
+                <p>{{ data.text2 }}</p>
+                <p>{{ data.text3 }}</p>
+                <button>Loe lähemalt</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue';
+import Data from '../data.json';
 
 export default {
     name: 'Services',
     components: {
         Navbar,
+    },
+    data() { 
+        return {
+            serviceData: Data,
+        }
     }
 }
 </script>
